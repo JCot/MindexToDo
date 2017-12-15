94f5bd79a8f1b87b621d4cab21d97f09f37d7bcc
Justin Cotner <jdc9622@rit.edu>
2017-12-15 17:14:05 -0500

    GetItem, AddItem, UpdateItem work

--- src/main/java/com/justin/Demo/ToDoController.java
+++ src/main/java/com/justin/Demo/ToDoController.java
@@ -1,10 +1,7 @@
 package com.justin.Demo;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -14,22 +11,45 @@ import java.util.ArrayList;
 import java.util.Date;
 
 @RestController
+@RequestMapping("/toDoList")
 public class ToDoController {
 
     private SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy/hh:mm:ss");
+    private ArrayList<ToDoItem> toDoList = new ArrayList<ToDoItem>();
 
-    @RequestMapping("/getAllItems")
+    private ToDoItem findItemByTitle(String title){
+        for(int i = 0; i < toDoList.size(); i++){
+            ToDoItem item = toDoList.get(i);
+            String curTitle = item.getTitle();
+            if(curTitle.equals(title)){
+                return item;
+            }
+        }
+
+        return null;
+    }
+
+    @GetMapping("/getAllItems")
     public ArrayList<ToDoItem> getAllItems(){
-        return new ArrayList<ToDoItem>();
+        return toDoList;
     }
 
-    @RequestMapping("/getItem")
-    public ToDoItem getOneItem(@RequestParam(value="title") String title){
+    @GetMapping("/getItem")
+    public ResponseEntity getOneItem(@RequestParam(value="title") String title){
         //TODO get from local store
-        return new ToDoItem(title,"", new Date());
+        if(toDoList.size() == 0){
+            ToDoItem newItem = new ToDoItem("Test", "Test Description", new Date());
+            toDoList.add(newItem);
+        }
+        ToDoItem item = findItemByTitle(title);
+        if(item != null) {
+            return ResponseEntity.ok(item);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping("/addItem")
     public ResponseEntity addItem(@RequestParam(value="title") String title,
                                   @RequestParam(value="desc") String desc,
                                   @RequestParam(value="dueDate") String dueDate){
@@ -40,24 +60,25 @@ public class ToDoController {
         catch(ParseException e){
             System.out.println("Error attempting to parse date string");
         }
-        //TODO put in local store/database
 
         if(newItem != null) {
+            toDoList.add(newItem);
+
             URI location = ServletUriComponentsBuilder
                     .fromCurrentRequest().path("/{title}")
                     .buildAndExpand(newItem.getTitle()).toUri();
-            return ResponseEntity.created(location).build();
+            return ResponseEntity.created(location).body(newItem);
         }
         else{
             return ResponseEntity.badRequest().build();
         }
     }
 
-    @RequestMapping(method = RequestMethod.PATCH)
+    @PatchMapping("/updateItem")
     public ResponseEntity updateItem(@RequestParam(value="title") String title,
                            @RequestParam(value="desc", defaultValue = "") String desc,
                            @RequestParam(value="dueDate", defaultValue = "") String dueDate){
-        ToDoItem item = getOneItem(title);
+        ToDoItem item = findItemByTitle(title);
         if(item != null) {
             if (!desc.equals("") || !dueDate.equals("")) {
                 if (!desc.equals("")) {
@@ -72,9 +93,8 @@ public class ToDoController {
                     }
                 }
 
-                //TODO update data store
-
-                return ResponseEntity.ok().build();
+                item = findItemByTitle(title);
+                return ResponseEntity.ok(item);
             }
         }
 
